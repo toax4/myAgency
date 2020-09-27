35aca6fb78c7992dbd3587165b811b93c38c1bc1
Bobo <adrien.bock83@gmail.com>
2020-09-27 17:07:10 +0200

    Ref card de app vers home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,32 +8,4 @@ import { green, red } from 'color-name';
 })
 export class AppComponent {
 	title = 'myAgency';
-
-	proprerties = [
-		{
-			title: "Ma super maison",
-			category: "Maison",
-			sold: true
-		},
-		{
-			title: "Petit appartement",
-			category: "Appartement",
-			sold: false
-		},
-		{
-			title: "Belle villa",
-			category: "Maison",
-			sold: true
-		},
-	];
-
-
-	sold = true;
-	getSoldValue(index) {
-		if(this.proprerties[index].sold) {
-			return "red";
-		}
-
-		return "green";
-	}
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
+	ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
 
+	proprerties = [
+		{
+			title: "Ma super maison",
+			category: "Maison",
+			sold: true
+		},
+		{
+			title: "Petit appartement",
+			category: "Appartement",
+			sold: false
+		},
+		{
+			title: "Belle villa",
+			category: "Maison",
+			sold: true
+		},
+	];
+
+
+	sold = true;
+	getSoldValue(index) {
+		if(this.proprerties[index].sold) {
+			return "red";
+		}
+
+		return "green";
+	}
 }
