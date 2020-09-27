745a4e3e4f0922d51b77410ad453452282c558d9
Bobo <adrien.bock83@gmail.com>
2020-09-27 16:59:00 +0200

    Generation components Home et AdminDashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
+import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
