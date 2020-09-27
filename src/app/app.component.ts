57c57d552c0dd81c267c23aa1aa764b664417980
Bobo <adrien.bock83@gmail.com>
2020-09-27 16:43:53 +0200

    Conditions Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { green, red } from 'color-name';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'myAgency';
+	title = 'myAgency';
+
+	sold = true;
+	getSoldValue() {
+		if(this.sold) {
+			return "red";
+		}
+
+		return "green";
+	}
 }
